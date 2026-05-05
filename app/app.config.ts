340410464c55a44ab8cfcd29731ac291a13c165e
kirpalmakanga <makanga.kirpal@gmail.com>
2026-05-05 04:34:39 +0200

    updated theme colors

--- app/app.config.ts
+++ app/app.config.ts
@@ -1,8 +1,8 @@
 export default defineAppConfig({
     ui: {
         colors: {
-            primary: 'indigo',
-            neutral: 'zinc'
+            primary: 'slate',
+            neutral: 'gray'
         }
     }
 });

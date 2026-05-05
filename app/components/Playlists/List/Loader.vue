38cc266212dfc4188199d0d965f249e3392fb03c
kirpalmakanga <makanga.kirpal@gmail.com>
2026-05-05 18:01:21 +0200

    removed cursor styles

--- app/components/Playlists/List/Loader.vue
+++ app/components/Playlists/List/Loader.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="p-6">
         <UCard variant="soft" :ui="{ body: 'flex items-center' }">
-            <div class="flex grow overflow-hidden p-4 gap-4 cursor-pointer">
+            <div class="flex grow overflow-hidden p-4 gap-4">
                 <USkeleton class="aspect-video h-28" />
 
                 <div class="flex flex-col grow"><USkeleton class="h-4 w-full" /></div>

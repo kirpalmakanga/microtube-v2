c5012817b3e9ed015084cf03313a1741a601f7f5
kirpalmakanga <makanga.kirpal@gmail.com>
2026-05-07 23:14:08 +0200

    DefaultHeader: removed comments

--- app/components/layout/DefaultHeader.vue
+++ app/components/layout/DefaultHeader.vue
@@ -1,25 +1,6 @@
 <script setup lang="ts">
-const route = useRoute();
 const authStore = useAuthStore();
 const { picture } = storeToRefs(authStore);
-// import { useAppTitle } from '../store/app';
-// import { useAuth } from '../store/user';
-
-// const DefaultHeader = () => {
-//     const location = useLocation();
-//     const [user, { signIn, signOut }] = useAuth();
-//     const [isMenuOpen, setMenuState] = createSignal(false);
-
-//     const title = useAppTitle();
-
-//     const handleClickUser = () => setMenuState(!isMenuOpen());
-
-//     const handleLogging = () => {
-//         if (user.isSignedIn) signOut();
-//         else signIn();
-
-//         setMenuState(false);
-//     };
 </script>
 
 <template>
@@ -33,17 +14,6 @@ const { picture } = storeToRefs(authStore);
         </template>
 
         <template #right>
-            <!-- <UTooltip text="Open on GitHub">
-                <UButton
-                    to="https://github.com/nuxt-ui-templates/starter"
-                    target="_blank"
-                    icon="i-simple-icons-github"
-                    aria-label="GitHub"
-                    color="neutral"
-                    variant="ghost"
-                />
-            </UTooltip> -->
-
             <SearchForm />
 
             <UColorModeButton />

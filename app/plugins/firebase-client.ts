aa19b9798137b81c3f56a815f4ffcc92a859f920
kirpalmakanga <makanga.kirpal@gmail.com>
2026-05-07 18:37:23 +0200

    plugins: removed firebase client init

--- app/plugins/firebase-client.ts
+++
@@ -1,9 +0,0 @@
-import { initializeApp } from 'firebase/app';
-
-export default defineNuxtPlugin(() => {
-    const {
-        public: { firebaseConfig }
-    } = useRuntimeConfig();
-
-    initializeApp(firebaseConfig);
-});

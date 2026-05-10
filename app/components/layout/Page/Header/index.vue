a84f854109905b7ab2b889c2e14846f1d0747454
kirpalmakanga <makanga.kirpal@gmail.com>
2026-05-10 05:45:20 +0200

    LayoutPageHeader: added ellipsis to content

--- app/components/layout/Page/Header/index.vue
+++ app/components/layout/Page/Header/index.vue
@@ -2,7 +2,7 @@
     <div class="flex bg-elevated/80 p-6 gap-6 shadow">
         <slot name="thumbnail" />
 
-        <div class="flex flex-col grow gap-2">
+        <div class="flex flex-col grow gap-2 overflow-hidden">
             <slot name="content" />
         </div>
     </div>

456c8800803b40873700003b9ffb4f5f715632f1
kirpalmakanga <makanga.kirpal@gmail.com>
2026-05-12 21:11:38 +0200

    layouts/default: added container width

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -7,7 +7,7 @@ const { isSignedIn } = storeToRefs(authStore);
     <LayoutDefaultHeader />
 
     <UMain class="flex flex-col">
-        <div class="flex flex-col grow">
+        <div class="flex flex-col grow w-full max-w-(--ui-container) mx-auto">
             <NuxtPage />
         </div>
 

581d18327c56958430649e66156de4f6b7e828dc
kirpalmakanga <makanga.kirpal@gmail.com>
2026-05-13 02:37:34 +0200

    LayoutPageHeader: rounded corners on big screens

--- app/components/layout/Page/Header/index.vue
+++ app/components/layout/Page/Header/index.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="flex bg-elevated/80 p-6 gap-6 shadow">
+    <div class="flex bg-elevated/80 p-6 gap-6 shadow xl:rounded-b-lg">
         <slot name="thumbnail" />
 
         <div class="flex flex-col grow gap-2 overflow-hidden">

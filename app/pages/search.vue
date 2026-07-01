f4349d2049289aec89a00337da11962651b7f3e6
kirpalmakanga <makanga.kirpal@gmail.com>
2026-07-01 20:37:34 +0200

    pages/search: refactoring route handling

--- app/pages/search.vue
+++ app/pages/search.vue
@@ -1,24 +1,18 @@
 <script setup lang="ts">
-import type { SearchFormData } from '~/components/Search/Form.vue';
+import { useRouteQuery } from '@vueuse/router';
 
-const route = useRoute();
-
-const search = computed(() => {
-    const forMine = parseInt(route.query.forMine as string);
-
-    return {
-        query: route.query.query as string,
-        forMine: isNaN(forMine) ? 0 : forMine
-    } as SearchFormData;
-});
+const query = useRouteQuery('query', '');
+const forMine = useRouteQuery('forMine', '0', { transform: Number });
 </script>
 
 <template>
     <div class="flex flex-col grow">
-        <div class="p-4 md:p-6 bg-slate-600/80"><SearchForm /></div>
+        <div class="p-4 md:p-6 bg-slate-600/80">
+            <SearchForm />
+        </div>
 
-        <template v-if="search.query">
-            <SearchResults v-bind="search" />
+        <template v-if="query">
+            <SearchResults :query="query" :forMine="forMine" />
         </template>
     </div>
 </template>

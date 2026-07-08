d93796275f161934b36e1e641d98dc604aebb42c
kirpalmakanga <makanga.kirpal@gmail.com>
2026-07-08 19:10:23 +0200

    useAuthStore: switched to cookie storage

--- app/composables/store/use-auth-store.ts
+++ app/composables/store/use-auth-store.ts
@@ -66,7 +66,9 @@ export const useAuthStore = defineStore(
     },
     {
         persist: {
-            storage: piniaPluginPersistedstate.cookies()
+            storage: piniaPluginPersistedstate.cookies({
+                expires: new Date(Date.now() + 6 * 30 * 24 * 60 * 60 * 1000) // 6 months
+            })
         }
     }
 );
